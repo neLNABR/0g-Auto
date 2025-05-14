2c2944f11f1d140ce5f244c795b480eebb42b435
neLNABR <kuznecovalesya988@rambler.ru>
2025-05-14 22:41:31 +0300

    Update storagescan_deploy.py

--- src/model/ZeroG/storagescan_deploy.py
+++ src/model/ZeroG/storagescan_deploy.py
@@ -15,7 +15,7 @@ from src.utils.config import Config
 from src.utils.constants import EXPLORER_URL_0G
 
 STORAGE_SCAN_CONTRACT = "0x56A565685C9992BF5ACafb940ff68922980DBBC5"
-CHAIN_ID = 80087
+CHAIN_ID = 16601
 
 
 @retry_async(default_value=False)
